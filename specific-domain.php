b8efcfbec576d33b262dabdae73fe99f61ab42ef
mohdshakeel <mohammadshakeel77@gmail.com>
2018-08-22 00:55:28 +0530

    Create specific-domain.php

---
+++ specific-domain.php
@@ -0,0 +1,16 @@
+add_filter('wc_memberships_grant_access_to_free_membership','mohammad_restrict_domains', 10, 3);
+function mohammad_restrict_domains( $bool, $user_args ) {
+  //echo $user_args['user_id'];
+  //print_r($user_args);
+	$whitelist = array('agbarr.co.uk');
+    $user_data = get_user_by('id', $user_args['user_id']); print_r($user_data);
+    $email = $user_data->user_email; //echo $email; die($email);
+	if ( is_email($email) ) {
+		$parts = explode('@', $email);
+		$domain = $parts[count($parts)-1];
+		if ( !in_array(strtolower($domain), $whitelist) ) {
+			$bool = false;
+		}
+	}
+	return $bool;
+}
