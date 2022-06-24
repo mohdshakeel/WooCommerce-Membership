4a68747b07650d05ab136d9e9d9b8c2de1c38f5c
mohdshakeel <mohammadshakeel77@gmail.com>
2022-06-24 19:51:15 +0530

    Update specific-domain.php
    THIS FUNCTION WILL ALLOW TO REGISTRER SPECIFIC DOMAIN EMAIL'S USERS

--- specific-domain.php
+++ specific-domain.php
@@ -1,8 +1,8 @@
+## THIS FUNCTION WILL ALLOW TO REGISTRER SPECIFIC DOMAIN EMAIL'S USERS.
 add_filter('wc_memberships_grant_access_to_free_membership','mohammad_restrict_domains', 10, 3);
 function mohammad_restrict_domains( $bool, $user_args ) {
-  //echo $user_args['user_id'];
-  //print_r($user_args);
-	$whitelist = array('agbarr.co.uk');
+  
+	$whitelist = array('agbarr.co.uk'); // here you can add more domain to allow for user registration
     $user_data = get_user_by('id', $user_args['user_id']); print_r($user_data);
     $email = $user_data->user_email; //echo $email; die($email);
 	if ( is_email($email) ) {
